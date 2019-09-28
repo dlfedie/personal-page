a5a1bdb08cd52141485a8a89eec1fcc60846da47
Dustin Fedie <dlfedie@gmail.com>
2019-09-28 11:57:35 -0500

    adds base setup

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Dustin Fedie</h1>
+        </header>
+        <body>
+          <div>
+            <p>some text</p>
+          </div>
+        </body>
+      </div>
+    );
+  }
+}
+
+export default App;
