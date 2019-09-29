05d7ba57f14f39a65e1297b1c8d028d9a5209616
Dustin Fedie <dlfedie@gmail.com>
2019-09-29 14:36:56 -0500

    adds more info and headshot

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import myPic from '../../images/headshot.JPG';
 
 class App extends Component {
   render() {
@@ -8,11 +9,25 @@ class App extends Component {
         <header className="App-header">
           <h1>Dustin Fedie</h1>
         </header>
-        <body>
+
+        <div className="textSpot">
+          <h2 className="headerEmail">dlfedie@gmail.com</h2>
+          <img src={myPic} alt="Dustin" className="headshot" />
           <div>
-            <p>some text</p>
+            <a href="https://www.linkedin.com/in/dustin-fedie/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </div>
-        </body>
+          <div>
+            <a href="https://github.com/dlfedie" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </div>
+          <div className="bottomBox">
+            <h3>Projects I've worked on:</h3>
+            <p><a href="https://evening-atoll-19955.herokuapp.com/#/home" target="_blank" rel="noopener noreferrer">ReadMe:</a> a mobile-first app to track reading progress and friend recommendations.</p>
+            <p><a href="https://secure-retreat-26616.herokuapp.com/" target="_blank" rel="noopener noreferrer">To Do List:</a> a small web app to track tasks.</p>
+            <p><a href="https://obscure-tor-27806.herokuapp.com/" target="_blank" rel="noopener noreferrer">Calculator:</a> one of my first projects of creating a server-side calculator.</p>
+            <p>Note: these are all hosted on Heroku, so if they don't load right away, give it a moment to fire up the server!</p>
+          </div>
+          <footer className="footer">&copy; Dustin Fedie, including photos</footer>
+        </div>
       </div>
     );
   }
